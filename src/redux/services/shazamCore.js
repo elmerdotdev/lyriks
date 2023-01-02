1250da847ba58f65b977fe99bdc51d7fd25e04ba
Elmer <contact@elmer.dev>
2023-01-02 10:22:54 +0800

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '9957b80c01msh0f5657f0df8ec89p1e1240jsnde9e8ebc3ec5');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
